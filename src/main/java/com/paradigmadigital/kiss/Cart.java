cfb605393e79adc55e522eee8d6d8e193902a3a9
Ismail Ismailov <ismail2ov@gmail.com>
2023-03-13 05:15:55 +0100

    Increment cohesion

--- src/main/java/com/paradigmadigital/kiss/Cart.java
+++ src/main/java/com/paradigmadigital/kiss/Cart.java
@@ -1,5 +1,7 @@
 package com.paradigmadigital.kiss;
 
+import com.paradigmadigital.kiss.exception.CartException;
+
 public class Cart {
 
     public static final int MIN_PURCHASE_AMOUNT = 30;
@@ -15,5 +17,23 @@ public class Cart {
 
     public boolean checkStock() {
         return true;
+    }
+
+    public void validate() {
+        if (getNumProducts() <= 0) {
+            throw new CartException("The cart is empty");
+        }
+
+        if (getTotal() < MIN_PURCHASE_AMOUNT) {
+            throw new CartException("Does not reach the minimum required for the cart");
+        }
+
+        if (getTotal() >= MAX_PURCHASE_AMOUNT) {
+            throw new CartException("You have exceeded the maximum total allowed for a purchase");
+        }
+
+        if (!checkStock()) {
+            throw new CartException("No hay stock");
+        }
     }
 }
--- src/main/java/com/paradigmadigital/kiss/Checkout.java
+++ src/main/java/com/paradigmadigital/kiss/Checkout.java
@@ -1,9 +1,5 @@
 package com.paradigmadigital.kiss;
 
-import com.paradigmadigital.kiss.exception.CartException;
-import com.paradigmadigital.kiss.exception.DeliveryException;
-import com.paradigmadigital.kiss.exception.PaymentException;
-
 public class Checkout {
 
     Cart cart;
@@ -17,48 +13,11 @@ public class Checkout {
     }
 
     public boolean validate() {
-        validateCart();
-        validateDelivery();
-        validatePayment();
+        cart.validate();
+        delivery.validate();
+        payment.validate();
 
         return true;
     }
 
-    private void validatePayment() {
-        if (!payment.hasPaymentMethods()) {
-            throw new PaymentException("No payment methods");
-        }
-
-        if (!payment.isCompulsiveBuyer()) {
-            throw new PaymentException("Are you a compulsive buyer signed");
-        }
-    }
-
-    private void validateDelivery() {
-        if (!delivery.checkAddress()) {
-            throw new DeliveryException("Invalid address");
-        }
-
-        if (!delivery.hasCarrier()) {
-            throw new DeliveryException("No carrier");
-        }
-    }
-
-    private void validateCart() {
-        if (cart.getNumProducts() <= 0) {
-            throw new CartException("The cart is empty");
-        }
-
-        if (cart.getTotal() < Cart.MIN_PURCHASE_AMOUNT) {
-            throw new CartException("Does not reach the minimum required for the cart");
-        }
-
-        if (cart.getTotal() >= Cart.MAX_PURCHASE_AMOUNT) {
-            throw new CartException("You have exceeded the maximum total allowed for a purchase");
-        }
-
-        if (!cart.checkStock()) {
-            throw new CartException("No hay stock");
-        }
-    }
 }
