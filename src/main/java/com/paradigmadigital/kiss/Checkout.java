4288d3a6750660f8cb3d5321d7097a5ee01e05c5
Ismail Ismailov <ismail2ov@gmail.com>
2023-03-13 04:07:16 +0100

    Replace Nested Conditional with Guard Clauses

--- src/main/java/com/paradigmadigital/kiss/Checkout.java
+++ src/main/java/com/paradigmadigital/kiss/Checkout.java
@@ -18,47 +18,38 @@ public class Checkout {
 
     public boolean validateCart() {
 
-        if (cart.getNumProducts() > 0) {
-
-            if (cart.getTotal() >= Cart.MIN_PURCHASE_AMOUNT) {
-
-                if (cart.getTotal() < Cart.MAX_PURCHASE_AMOUNT) {
-
-                    if (cart.checkStock()) {
-
-                        if (delivery.checkAddress()) {
-
-                            if (delivery.hasCarrier()) {
-
-                                if (payment.hasPaymentMethods()) {
-
-                                    if (payment.isCompulsiveBuyer()) {
-
-                                        return true;
-
-                                    } else {
-                                        throw new PaymentException("Are you a compulsive buyer signed");
-                                    }
-                                } else {
-                                    throw new PaymentException("No payment methods");
-                                }
-                            } else {
-                                throw new DeliveryException("No carrier");
-                            }
-                        } else {
-                            throw new DeliveryException("Invalid address");
-                        }
-                    } else {
-                        throw new CartException("No hay stock");
-                    }
-                } else {
-                    throw new CartException("You have exceeded the maximum total allowed for a purchase");
-                }
-            } else {
-                throw new CartException("Does not reach the minimum required for the cart");
-            }
-        } else {
+        if (cart.getNumProducts() <= 0) {
             throw new CartException("The cart is empty");
         }
+
+        if (cart.getTotal() < Cart.MIN_PURCHASE_AMOUNT) {
+            throw new CartException("Does not reach the minimum required for the cart");
+        }
+
+        if (cart.getTotal() >= Cart.MAX_PURCHASE_AMOUNT) {
+            throw new CartException("You have exceeded the maximum total allowed for a purchase");
+        }
+
+        if (!cart.checkStock()) {
+            throw new CartException("No hay stock");
+        }
+
+        if (!delivery.checkAddress()) {
+            throw new DeliveryException("Invalid address");
+        }
+
+        if (!delivery.hasCarrier()) {
+            throw new DeliveryException("No carrier");
+        }
+
+        if (!payment.hasPaymentMethods()) {
+            throw new PaymentException("No payment methods");
+        }
+
+        if (!payment.isCompulsiveBuyer()) {
+            throw new PaymentException("Are you a compulsive buyer signed");
+        }
+
+        return true;
     }
 }
